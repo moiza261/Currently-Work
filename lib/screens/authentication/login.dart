81a732acc4e2eafe42561d5d0a74b82cb06d4de6
Moiz Ahmad <moiza261@gmail.com>
2022-01-25 13:10:26 +0500

    Login Page Complete

--- lib/screens/authentication/login.dart
+++ lib/screens/authentication/login.dart
@@ -9,6 +9,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool ishidepassword = true;
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   @override
@@ -27,47 +28,181 @@ class _LoginState extends State<Login> {
             )),
       ),
       body: SafeArea(
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          color: Colors.white,
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 10,
-              ),
-              const Image(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            const Center(
+              child: Image(
                 image: AssetImage('assets/Group 30.png'),
-                height: 100,
+                height: 140,
               ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  width: MediaQuery.of(context).size.width / 1,
-                  height: MediaQuery.of(context).size.height / 1.45,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5.0),
-                      color: Colors.white,
-                      boxShadow: const [
-                        BoxShadow(
-                            color: Color.fromARGB(255, 180, 104, 99),
-                            blurRadius: 4,
-                            spreadRadius: 1,
-                            blurStyle: BlurStyle.outer,
-                            offset: Offset(0, 1)),
-                      ]),
+            ),
+            Expanded(
+              child: Container(
+                width: MediaQuery.of(context).size.width / 1.15,
+                height: MediaQuery.of(context).size.height,
+                decoration:
+                    const BoxDecoration(color: Colors.white, boxShadow: [
+                  BoxShadow(
+                      color: Colors.red,
+                      blurRadius: 5.0,
+                      blurStyle: BlurStyle.outer),
+                ]),
+                child: SingleChildScrollView(
                   child: Column(
-                    children: const [
-                      Text(
+                    children: [
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const Text(
                         'HELLO',
-                        style: TextStyle(fontFamily: ""),
-                      )
+                        style: TextStyle(
+                            fontFamily: "Poppins",
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      const Text(
+                        'Please Login to your account',
+                        style: TextStyle(
+                            color: Colors.grey, fontFamily: "Poppins"),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15, right: 15),
+                            child: TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: "Driver Email Address",
+                                labelStyle: TextStyle(
+                                    color: Colors.purple,
+                                    fontSize: 16,
+                                    fontFamily: "Poppins"),
+                                hintText: "Enter Your Email",
+                                suffixIcon: Icon(
+                                  Icons.mail,
+                                  color: Colors.purple,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15, right: 15),
+                            child: TextFormField(
+                              obscureText: ishidepassword,
+                              decoration: InputDecoration(
+                                labelText: "Password",
+                                labelStyle: TextStyle(
+                                    color: Colors.purple,
+                                    fontSize: 16,
+                                    fontFamily: "Poppins"),
+                                hintText: "Enter Your Password",
+                                suffixIcon: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      ishidepassword = !ishidepassword;
+                                    });
+                                  },
+                                  icon: Icon(
+                                    ishidepassword
+                                        ? Icons.visibility_off
+                                        : Icons.visibility,
+                                    color: Colors.purple,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: InkWell(
+                          onTap: () {},
+                          child: const Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                                color: Colors.purple, fontFamily: "Poppins"),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width / 1.30,
+                        height: MediaQuery.of(context).size.height / 12.5,
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(colors: [
+                            Colors.purple,
+                            Colors.red,
+                          ]),
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        child: Center(
+                          child: TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              'LOGIN',
+                              style: TextStyle(
+                                  color: Colors.white, fontFamily: "Poppins"),
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      const Text(
+                        'Or Login using social media',
+                        style: TextStyle(
+                          fontFamily: "Poppins",
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Image(
+                            image: AssetImage('assets/Path 2477.png'),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Image(
+                            image: AssetImage('assets/Path 2478.png'),
+                          ),
+                          SizedBox(
+                            width: 30,
+                          ),
+                          Image(
+                            image: AssetImage('assets/Union 6.png'),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
